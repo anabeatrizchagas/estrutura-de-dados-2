3e445386e5a320c595c98a68300df06d2f1d9131
Ana Beatriz R. Chagas <121564402+anabeatrizchagas@users.noreply.github.com>
2023-04-11 09:39:30 -0300

    new file:   02-grafo-p-d-m-adj.c new file:   02-grafo-p-d-m-adj.h

---
+++ 02-grafo-p-d-m-adj.c
@@ -0,0 +1,182 @@
+// importando header
+
+#include "02-grafo-p-d-m-adj.h"
+
+// implementando funcoes
+
+bool inicializaGrafo(GRAFO* g, int nv)
+{
+    if(nv < 1) // numero de vertices menor que o minimo
+    {
+        fprintf(stderr, "Erro: inicializaGrafo(GRAFO* g, int nv) - nv < 1\n");
+        return (false);
+
+    } else if (nv > MAX_NUM_VERTICES) { // numero de vertices maior que o maximo
+
+        fprintf(stderr, "Erro: inicializaGrafo(GRAFO* g, int nv) - nv > NUM_MAX_VERTICES\n");
+        return (false);
+
+    } else { // numero de vertices valido
+
+        int i, j;
+
+        for(i = 1; i <= nv; i++)
+        {
+            for(j = 1; j <= nv; j++) g->m[i][j] = AN;
+        }
+
+        g->numArestas = 0;
+        g->numVertices = nv;
+
+    }
+
+    return (true);
+
+}
+
+bool insereAresta(GRAFO* g, int v1, int v2, PESO p)
+{
+    if(v1 < 1 || v2 < 1) // indice menor que o minimo
+    {
+        fprintf(stderr, "Erro: insereAresta(GRAFO* g, int v1, int v2) - v1 ou v2 < 1\n");
+        return (false);
+
+    } else if (v1 > g->numVertices || v2 > g->numVertices) { // indice maior que o maximo
+
+        fprintf(stderr, "Erro: insereAresta(GRAFO* g, int v1, int v2) - v1 ou v2 > g->numVertices\n");
+        return (false);
+
+    } else if(p < MIN_PESO) { // peso menor que o minimo
+
+        fprintf(stderr, "Erro: insereAresta(GRAFO* g, int v1, int v2) - p < MIN_PESO \n");
+        return (false);
+
+    } else if(p > MAX_PESO) { // peso maior que o maximo
+
+        fprintf(stderr, "Erro: insereAresta(GRAFO* g, int v1, int v2) - p > MAX_PESO\n");
+        return (false);
+
+    } else { // indice e peso validos
+
+        if(g->m[v1][v2] == AN) g->numArestas++;
+        g->m[v1][v2] = p;
+        return (true);
+
+    }
+
+}
+
+bool existeAresta(GRAFO* g, int v1, int v2)
+{
+    if(v1 < 1 || v2 < 1) // indice menor que o minimo
+    {
+        fprintf(stderr, "Erro: existeAresta(GRAFO* g, int v1, int v2) - v1 ou v2 < 1");
+
+    } else if (v1 > g->numVertices || v2 > g->numVertices) { // indice maior que o maximo
+
+        fprintf(stderr, "Erro: existeAresta(GRAFO* g, int v1, int v2) - v1 ou v2 > g->numVertices");
+
+    } else { // indice valido
+
+        if(g->m[v1][v2] != AN) return (true);
+        else return (false);
+
+    }
+
+}
+
+bool retiraAresta(GRAFO* g, int v1, int v2)
+{
+    if(v1 < 1 || v2 < 1) // indice menor que o minimo
+    {
+        fprintf(stderr, "Erro: retiraAresta(GRAFO* g, int v1, int v2, bool direc) - v1 ou v2 < 1");
+        return (false);
+
+    } else if (v1 > g->numVertices || v2 > g->numVertices) { // indice maior que o maximo
+
+        fprintf(stderr, "Erro: retiraAresta(GRAFO* g, int v1, int v2, bool direc) - v1 ou v2 > g->numVertices");
+        return (false);
+
+    } else { // indice valido
+
+        if(g->m[v1][v2] != AN) g->numArestas--;
+        g->m[v1][v2] = AN;
+
+    }
+
+    return (true);
+
+
+}
+
+bool liberaGrafo(GRAFO* g)
+{
+    g->numArestas = 0;
+    g->numVertices = 0;
+
+    return (true);
+ 
+}
+
+void imprimeGrafo(GRAFO* g)
+{
+    int i, j;
+
+    for(i = 1; i <= g->numVertices; i++)
+    {
+        for(j = 1; j <= g->numVertices; j++) printf("%d ", g->m[i][j]);
+        printf("\n");
+
+    }
+
+}
+
+PESO retornaMin(GRAFO* g)
+{
+    if(g->numArestas == 0) // grafo vazio (sem pesos validos)
+    {
+        fprintf(stderr, "Erro: retornaMin(GRAFO* g) o grafo g indicado esta vazio\n");
+        
+    } else { // grafo com arestas (tem peso validos)
+
+        int i, j;
+        PESO resp = 999;
+
+        for(i = 1; i <= g->numVertices; i++)
+        {
+            for(j = 1; j <= g->numVertices; j++) 
+            {
+                if(g->m[i][j] != AN && g->m[i][j] < resp) resp = g->m[i][j];
+            }
+
+        }    
+
+        return (resp);
+
+    }
+
+}
+
+GRAFO grafoTransposto(GRAFO* g)
+{
+    int i, j;
+
+    GRAFO resp;
+    resp.numArestas = g->numArestas;
+    resp.numVertices = g->numVertices;
+
+    for(i = 1; i <= g->numVertices; i++)
+    {
+        for(j = 1; j <= g->numVertices; j++) resp.m[i][j] = g->m[j][i];
+    }
+
+    return (resp);
+
+}
+
+int main()
+{
+
+    return 0;
+    
+}
---
+++ 02-grafo-p-d-m-adj.h
@@ -0,0 +1,111 @@
+// ===================== GRAFO PONDERADO (COM PESO NAS ARESTAS) E DIRECIONADO =====================
+
+// importando pacotes
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// definindo tamanho padrao e peso da aresta nula
+
+#define MAX_NUM_VERTICES 20 // numero maximo de vertices que pode existir no grafo
+#define MAX_PESO 10 // peso maximo de uma aresta
+#define MIN_PESO 0 // peso minimo de uma aresta
+#define AN -1 // se a aresta nao existe, seu peso e -1
+
+// criando grafo a partir de uma matriz de adjacencia
+
+typedef int PESO;
+
+typedef struct 
+{
+    PESO m[MAX_NUM_VERTICES + 1][MAX_NUM_VERTICES + 1]; // vertices enumerados de 1 a MAX_NUM_VERTICES
+    int numArestas;
+    int numVertices;
+
+} GRAFO;
+
+// declarando funcoes
+
+/*
+
+    bool inicializaGrafo(GRAFO* g, int nv) recebe como parametro o endereco do
+    grafo g e um int nv que e a quantidade de vertices que tera nesse grafo. 
+    A funcao inicializa com AN uma matriz de adjacencia que representa o grafo.
+    Alem disso, tambem zera a o numero de vertices e arestas do grafo.
+
+*/
+
+bool inicializaGrafo(GRAFO* g, int nv);
+
+/*
+
+    bool insereAresta(GRAFO* g, int v1, int v2, PESO p) recebe o endereco do grafo g, um
+    int v1 e um int v2 que sao vertices e o PESO p, que e o peso da aresta que sera inserida.
+    Se v1 e v2 forem vertices validos, a funcao ira inserir uma aresta que sai de v1 e
+    chega em v2 com peso p. Ademais ira incrementar uma unidade no numero de arestas do grafo.
+
+*/
+
+bool insereAresta(GRAFO* g, int v1, int v2, PESO p);
+
+/*
+
+    bool existeAresta(GRAFO* g, int v1, int v2) recebe o endereco do grafo g, um
+    int v1 e um int v2 que sao vertices. Se v1 e v2 forem vertices validos, a funcao
+    ira checar se existe uma aresta que sai de v1 e chega em v2. Se existir, retornara
+    true, caso contrario, retornara false.
+
+*/
+
+bool existeAresta(GRAFO* g, int v1, int v2);
+
+/*
+
+    bool retiraAresta(GRAFO* g, int v1, int v2) recebe o endereco do grafo g, um
+    int v1 e um int v2 que sao vertices. Se v1 e v2 forem vertices validos e existir
+    uma aresta que sai de v1 e chega em v2, a funcao a retirara, decrementara o numero de arestas
+    e retornara true. Caso nao haja, retornara false.
+
+*/
+
+bool retiraAresta(GRAFO* g, int v1, int v2);
+
+/*
+
+    void liberaGrafo(GRAFO* g) recebe o endereco do grafo g e zera o numero de vertices
+    e arestas dele. Dessa forma, ele e reinicializado.
+
+*/
+
+
+bool liberaGrafo(GRAFO* g);
+
+/*
+
+    void imprimeGrafo(GRAFO* g) recebe o endereco do grafo g e imprime a matriz de
+    adjacencia que o representa. Nessa matriz, o AN  sinaliza que nao ha uma aresta 
+    que sai do vertice na linha i e chega no vertice na coluna j. Caso haja aresta,
+    a posicao guardara o peso dela.
+
+*/
+
+void imprimeGrafo(GRAFO* g);
+
+/*
+
+    PESO retornaMin(GRAFO* g) resebe o endereco do grafo g e retorna o menor peso desse grafo.
+    Caso o grafo nao possua arestas, a funcao retorna um erro informando que nao existe nenhuma aresta.
+
+*/
+
+PESO retornaMin(GRAFO* g);
+
+/*
+
+    GRAFO grafoTransposto(GRAFO* g) recebe o endereco do grafo g e retorna o seu grafo
+    transposto, ou seja, as colunas viram linhas e as linhas viram colunas.
+
+*/
+
+GRAFO grafoTransposto(GRAFO* g);
